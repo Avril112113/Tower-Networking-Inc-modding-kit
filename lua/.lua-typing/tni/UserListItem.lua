21d6eb8bbf2a345ef7550ae16279805db15256b0
Avril112113 <curtisdude12@gmail.com>
2025-12-24 19:08:21 +0000

    Major updates to Lua support and for suggested tni modding impl changes (#2)
    * Build release with debug info
    
    * Start overhaul of Lua impl
    
    * Check for invalid object ids and improved `instance_from_id_`
    
    * Expanded lua arg count limit
    
    * Added mod file access & Added more generated API
    
    * Updated generated API
    
    * Replace custom `lua_touserdata_t` with Lua's `luaL_checkudata`
    
    * Updated to use `Mod.api_v1` instead of `Mod.get_api_v1()`
    
    * Fix for Lua push of `Object<null>` (push nil instead)
    
    * Updated TNI headers
    
    * tni-mod-template to test new headers
    
    * Updated headers
    
    * Lua check `is_allowed_property` and lua error if not allowed
    
    * Push gd variant from GDObject and GDCallable
    + Misc supporting changes
    
    * Split GDObject and GDCallable into own files
    
    * Removed debug stuff, woops.
    
    * Added Lua function to Godot support
    
    * Use `setvbuf` to set how stdout is flushed.
    
    * Empty lua stack before running callback
    This prevents any lua stack overflow in-case of bugs
    
    * Updated/Improved tni-mod-template
    
    * Error if `push_gd_variant` doesn't push anything in `setup_lua_state`
    
    * Implement `searchpath` and `require` for ModFileSystem
    
    * Remove old headers
    
    * Add `Engine` global to Lua
    Allows access to other singletons from Lua
    
    * Updated `libluajit.a` and `zig-libluajit.a`, Added `luajit.h` and use `#include <lua.hpp>`
    
    LuaJIT from plctlab/LuaJIT on branch `v2.1-riscv64-pr`
    https://github.com/plctlab/LuaJIT/tree/a11f6e9f504cc911d3babe235d52405b6aadae4d
    
    * Added LuaJIT build instructions
    
    * Added godot array support
    
    * Renamed `Godot*` to `GD*` and improved __tostring
    
    * Added `return` with `luaL_error` calls.
    This may help the compiler a bit.
    
    * GDArray iter, fix incorrect `i`
    
    * GDArray, handle negative indices to prevent odd behavior from godot-sandbox
    
    * LuaJIT check if array value allowed on index
    
    * Updated headers
    
    * `push_gd_callable_metatable` and `push_gd_object_metatable`
    
    * Update `luamodpackage.cpp` for new headers
    
    * Lua, added globals `Object` and `Callable`
    
    * Lua assign on GDObject
    
    * Lua added global `create_node`
    
    * Added example of `AcceptDialog` to tni-mod-template
    
    * Added lua typing
    
    * Moved TNI includes to `include/tni`
    
    * Updated Lua high bw switches example
    
    * Updated tni headers & lua typing
    Should now be 100% of possible classes
    Removed instance ID from packed scene constants (still need to do this for other constant types)
    Also removed some local stuff which shouldn't have been there
    
    * Lua typing, fixed string enum missing quotes

---
+++ lua/.lua-typing/tni/UserListItem.lua
@@ -0,0 +1,16 @@
+---@meta _
+-- Generated API for game version 0.9.1
+
+---@class UserListItem : HBoxContainer
+---@field view_icon_texture Object # Constant value: <CompressedTexture2D#-9223369158337409671>
+---@field offline_icon_texture Object # Constant value: <CompressedTexture2D#-9223369157985088360>
+---@field is_header boolean
+---@field user User
+---@field usn_lbl Label
+---@field usn_btn Button
+---@field satiety_lbl Label
+---@field lowest_lbl Label
+---@field txticon_button TextureButton
+local UserListItem = {}
+
+function UserListItem.refresh_label() end
